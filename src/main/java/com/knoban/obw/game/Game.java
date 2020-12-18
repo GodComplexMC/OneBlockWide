9968360399bc428fc24691e40a56fe21f0c9e6b8
Realmm <jamesandrew107@hotmail.com>
2020-12-18 20:05:01 +1300

    Map now spawns barrier blocks 10 blocks above the highest spawned players height

--- src/main/java/com/knoban/obw/game/Game.java
+++ src/main/java/com/knoban/obw/game/Game.java
@@ -89,9 +89,13 @@ public final class Game implements Listener {
             case PRECOMBAT:
                 gameMap.getWorld().setPVP(false);
 
+                int highestSpawnedY = 0;
+
                 int index = 0, playerCount = Bukkit.getOnlinePlayers().size();
                 for(Player pl : Bukkit.getOnlinePlayers()) {
-                    pl.teleport(gameMap.getPlayerSpawnLocation(index++, playerCount));
+                    Location loc = gameMap.getPlayerSpawnLocation(index++, playerCount);
+                    pl.teleport(loc);
+                    if (loc.getBlockY() > highestSpawnedY) highestSpawnedY = loc.getBlockY();
                     pl.sendMessage(CC.NEON_BLUE + "Game> " + CC.MEDIUM_PURPLE + "Welcome to combat! PVP will be enabled soon.");
                     pl.sendMessage(CC.NEON_BLUE + "Game> " + CC.EGGSHELL + "We've given you some basic supplies, enjoy.");
                     pl.playSound(pl.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1F, 0.5F);
@@ -104,6 +108,8 @@ public final class Game implements Listener {
                     pl.setInvulnerable(false);
                     pl.setCollidable(false);
                 }
+
+                gameMap.generateCeiling(highestSpawnedY + 10);
 
                 countdown = initialPvPCountdown;
                 break;
@@ -229,7 +235,7 @@ public final class Game implements Listener {
         sb.append(CC.VIVID_BLUE_SKY);
         sb.append("(");
         sb.append(gameMap.getCompletedGenerationStages());
-        sb.append("/12)");
+        sb.append("/10)");
 
         return sb.toString();
     }
